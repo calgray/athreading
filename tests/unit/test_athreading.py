2b1ea1da364063b212ed25ee441fc2af3fca7b03
Callan Gray <cal.j.gray@gmail.com>
2025-01-01 18:27:19 +0800

    Test module version
    Signed-off-by: Callan Gray <cal.j.gray@gmail.com>

--- tests/unit/test_athreading.py
+++ tests/unit/test_athreading.py
@@ -1,8 +1,12 @@
 """Tests for the main module."""
 
+from importlib.metadata import version
+
 from athreading import __version__
 
+PACKAGE_VER = version("athreading")
 
-def test_version():
-    """Check that the version is acceptable."""
-    assert __version__ == "0.2.0"
+
+def test_module_version():
+    """Check that the module and package versions match."""
+    assert __version__ == PACKAGE_VER
